316d6c4780f84d3f7b0fcb778c8d0f7bc064327c
sh0ck-zy <sh0ck.zy.25@gmail.com>
2025-06-26 19:23:33 +0100

    refactor: simplify footer and update contact information

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,5 @@
-/** @type {import('postcss').Config} */
-export default {
-  plugins: {
-    'tailwindcss/nesting': {},
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+const config = {
+  plugins: ["@tailwindcss/postcss"],
 };
+
+export default config;
